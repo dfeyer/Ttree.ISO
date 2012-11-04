309e90a8f363c462247211eeadd64a9b1db9cdd1
Dominique Feyer <dfeyer@ttree.ch>
2012-11-04 16:18:32 +0100

    [TASK] Add a view helper to render country list

---
+++ Classes/Ttree/ISO/ViewHelpers/Form/SelectCountryViewHelper.php
@@ -0,0 +1,151 @@
+<?php
+namespace Ttree\ISO\ViewHelpers\Form;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Fluid".                 *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU Lesser General Public License, either version 3   *
+ * of the License, or (at your option) any later version.                 *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * This view helper generates a <select> dropdown list for ISO Countries
+ *
+ * @api
+ */
+class SelectCountryViewHelper extends \TYPO3\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\I18n\Translator
+	 */
+	protected $translator;
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'select';
+
+	/**
+	 * @var mixed
+	 */
+	protected $selectedValue = NULL;
+
+	/**
+	 * Initialize arguments.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerUniversalTagAttributes();
+		$this->registerTagAttribute('multiple', 'string', 'if set, multiple select field');
+		$this->registerTagAttribute('size', 'string', 'Size of input field');
+		$this->registerTagAttribute('disabled', 'string', 'Specifies that the input element should be disabled when the page loads');
+		$this->registerArgument('errorClass', 'string', 'CSS class to set if there are errors for this view helper', FALSE, 'f3-form-error');
+	}
+
+	/**
+	 * Render the tag.
+	 *
+	 * @return string rendered tag.
+	 * @api
+	 */
+	public function render() {
+		$name = $this->getName();
+		if ($this->hasArgument('multiple')) {
+			$name .= '[]';
+		}
+
+		$this->tag->addAttribute('name', $name);
+
+		$options = $this->getOptions();
+		if (empty($options)) {
+			$options = array('' => '');
+		}
+		$this->tag->setContent($this->renderOptionTags($options));
+
+		$this->setErrorClassAttribute();
+
+			// register field name for token generation.
+			// in case it is a multi-select, we need to register the field name
+			// as often as there are elements in the box
+		if ($this->hasArgument('multiple') && $this->arguments['multiple'] !== '') {
+			$this->renderHiddenFieldForEmptyValue();
+			for ($i = 0; $i < count($options); $i++) {
+				$this->registerFieldNameForFormTokenGeneration($name);
+			}
+		} else {
+			$this->registerFieldNameForFormTokenGeneration($name);
+		}
+
+		return $this->tag->render();
+	}
+
+	/**
+	 * Render the option tags.
+	 *
+	 * @param array $options the options for the form.
+	 * @return string rendered tags.
+	 */
+	protected function renderOptionTags($options) {
+		$output = '';
+
+		foreach ($options as $value => $label) {
+			$output .= $this->renderOptionTag($value, $label) . chr(10);
+		}
+		return $output;
+	}
+
+	/**
+	 * Render the option tags.
+	 *
+	 * @return array an associative array of options, key will be the value of the option tag
+	 * @throws \TYPO3\Fluid\Core\ViewHelper\Exception
+	 */
+	protected function getOptions() {
+		$options = array();
+
+		$options[0] = 'TODO';
+
+		return $options;
+	}
+
+	/**
+	 * Render one option tag
+	 *
+	 * @param string $value value attribute of the option tag (will be escaped)
+	 * @param string $label content of the option tag (will be escaped)
+	 * @return string the rendered option tag
+	 */
+	protected function renderOptionTag($value, $label) {
+		$output = '<option value="' . htmlspecialchars($value) . '"';
+
+		if ($this->hasArgument('translate')) {
+			$label = $this->getTranslatedLabel($value, $label);
+		}
+		$output .= '>' . htmlspecialchars($label) . '</option>';
+
+		return $output;
+	}
+
+	/**
+	 * Returns a translated version of the given label
+	 *
+	 * @param string $value option tag value
+	 * @param string $label option tag label
+	 * @return string
+	 * @throws \TYPO3\Fluid\Core\ViewHelper\Exception
+	 */
+	protected function getTranslatedLabel($value, $label) {
+		return 'TODO';
+	}
+}
+
+?>
