84dd6936da2c3b62284a646c1be2dacdd2e261aa
Dominique Feyer <dfeyer@ttree.ch>
2020-05-01 18:29:12 +0200

    Fix: always return string in __toString

--- Classes/Ttree/ISO/Domain/Model/Country.php
+++ Classes/Ttree/ISO/Domain/Model/Country.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Ttree\ISO\Domain\Model;
 
 /*                                                                        *
@@ -7,266 +8,296 @@ namespace Ttree\ISO\Domain\Model;
  *                                                                        */
 
 use TYPO3\Flow\Annotations as Flow,
-	Doctrine\ORM\Mapping as ORM;
+    Doctrine\ORM\Mapping as ORM;
 
 /**
  * ISO Country
  *
  * @Flow\Entity
  */
-class Country {
+class Country
+{
 
-	const STANDARD_ISO_3166   = 'ISO3166';
-	const STANDARD_ISO_3166_3 = 'ISO3166-3';
+    const STANDARD_ISO_3166 = 'ISO3166';
+    const STANDARD_ISO_3166_3 = 'ISO3166-3';
 
-	const PROPERTY_ISO_3166   = 'alpha2';
-	const PROPERTY_ISO_3166_3 = 'alpha3';
+    const PROPERTY_ISO_3166 = 'alpha2';
+    const PROPERTY_ISO_3166_3 = 'alpha3';
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=10 })
-	 * @Flow\Validate(type="NotEmpty")
-	 * @ORM\Column(length=10)
-	 */
-	protected $standard;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=10 })
+     * @Flow\Validate(type="NotEmpty")
+     * @ORM\Column(length=10)
+     */
+    protected $standard;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=2, "maximum"=2 })
-	 * @ORM\Column(nullable=true)
-	 * @ORM\Column(length=2)
-	 */
-	protected $alpha2;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=2, "maximum"=2 })
+     * @ORM\Column(nullable=true)
+     * @ORM\Column(length=2)
+     */
+    protected $alpha2;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=3, "maximum"=3 })
-	 * @Flow\Validate(type="NotEmpty")
-	 * @ORM\Column(length=3)
-	 */
-	protected $alpha3;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=3, "maximum"=3 })
+     * @Flow\Validate(type="NotEmpty")
+     * @ORM\Column(length=3)
+     */
+    protected $alpha3;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=2, "maximum"=4 })
-	 * @ORM\Column(nullable=true)
-	 * @ORM\Column(length=4)
-	 */
-	protected $alpha4;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=2, "maximum"=4 })
+     * @ORM\Column(nullable=true)
+     * @ORM\Column(length=4)
+     */
+    protected $alpha4;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @ORM\Column(nullable=true)
-	 * @ORM\Column(length=3)
-	 */
-	protected $numericCode;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @ORM\Column(nullable=true)
+     * @ORM\Column(length=3)
+     */
+    protected $numericCode;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=100 })
-	 * @Flow\Validate(type="NotEmpty")
-	 * @ORM\Column(length=100)
-	 */
-	protected $name;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=100 })
+     * @Flow\Validate(type="NotEmpty")
+     * @ORM\Column(length=100)
+     */
+    protected $name;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=100 })
-	 * @ORM\Column(nullable=true)
-	 * @ORM\Column(length=100)
-	 */
-	protected $commonName;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=100 })
+     * @ORM\Column(nullable=true)
+     * @ORM\Column(length=100)
+     */
+    protected $commonName;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=200 })
-	 * @ORM\Column(nullable=true)
-	 * @ORM\Column(length=200)
-	 */
-	protected $comment;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=200 })
+     * @ORM\Column(nullable=true)
+     * @ORM\Column(length=200)
+     */
+    protected $comment;
 
-	/**
-	 * @var \DateTime
-	 * @ORM\Column(nullable=true)
-	 */
-	protected $dateOfWithdrawn;
+    /**
+     * @var \DateTime
+     * @ORM\Column(nullable=true)
+     */
+    protected $dateOfWithdrawn;
 
-	/**
-	 * @var string
-	 * @Flow\Validate(type="Text")
-	 * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=200 })
-	 * @ORM\Column(nullable=true)
-	 */
-	protected $officialName;
+    /**
+     * @var string
+     * @Flow\Validate(type="Text")
+     * @Flow\Validate(type="StringLength", options={ "minimum"=1, "maximum"=200 })
+     * @ORM\Column(nullable=true)
+     */
+    protected $officialName;
 
-	/**
-	 * @Flow\Inject
-	 * @var \Ttree\ISO\Domain\Service\CountryService
-	 */
-	protected $countryService;
+    /**
+     * @Flow\Inject
+     * @var \Ttree\ISO\Domain\Service\CountryService
+     */
+    protected $countryService;
 
-	/**
-	 * @param string $standard
-	 */
-	public function setStandard($standard) {
-		$this->standard = $standard;
-	}
+    /**
+     * @param string $standard
+     */
+    public function setStandard($standard)
+    {
+        $this->standard = $standard;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getStandard() {
-		return $this->standard;
-	}
+    /**
+     * @return string
+     */
+    public function getStandard()
+    {
+        return $this->standard;
+    }
 
-	/**
-	 * @param string $comment
-	 */
-	public function setComment($comment) {
-		$this->comment = $comment;
-	}
+    /**
+     * @param string $comment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getComment() {
-		return $this->comment;
-	}
+    /**
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
 
-	/**
-	 * @param string $alpha2
-	 */
-	public function setAlpha2($alpha2) {
-		$this->alpha2 = $alpha2;
-	}
+    /**
+     * @param string $alpha2
+     */
+    public function setAlpha2($alpha2)
+    {
+        $this->alpha2 = $alpha2;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getAlpha2() {
-		return $this->alpha2;
-	}
+    /**
+     * @return string
+     */
+    public function getAlpha2()
+    {
+        return $this->alpha2;
+    }
 
-	/**
-	 * @param string $alpha3
-	 */
-	public function setAlpha3($alpha3) {
-		$this->alpha3 = $alpha3;
-	}
+    /**
+     * @param string $alpha3
+     */
+    public function setAlpha3($alpha3)
+    {
+        $this->alpha3 = $alpha3;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getAlpha3() {
-		return $this->alpha3;
-	}
+    /**
+     * @return string
+     */
+    public function getAlpha3()
+    {
+        return $this->alpha3;
+    }
 
-	/**
-	 * @param string $alpha4
-	 */
-	public function setAlpha4($alpha4) {
-		$this->alpha4 = $alpha4;
-	}
+    /**
+     * @param string $alpha4
+     */
+    public function setAlpha4($alpha4)
+    {
+        $this->alpha4 = $alpha4;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getAlpha4() {
-		return $this->alpha4;
-	}
+    /**
+     * @return string
+     */
+    public function getAlpha4()
+    {
+        return $this->alpha4;
+    }
 
-	/**
-	 * @param \DateTime $dateOfWithdrawn
-	 */
-	public function setDateOfWithdrawn($dateOfWithdrawn) {
-		$this->dateOfWithdrawn = $dateOfWithdrawn;
-	}
+    /**
+     * @param \DateTime $dateOfWithdrawn
+     */
+    public function setDateOfWithdrawn($dateOfWithdrawn)
+    {
+        $this->dateOfWithdrawn = $dateOfWithdrawn;
+    }
 
-	/**
-	 * @return \DateTime
-	 */
-	public function getDateOfWithdrawn() {
-		return $this->dateOfWithdrawn;
-	}
+    /**
+     * @return \DateTime
+     */
+    public function getDateOfWithdrawn()
+    {
+        return $this->dateOfWithdrawn;
+    }
 
-	/**
-	 * @param string $name
-	 */
-	public function setName($name) {
-		$this->name = $name;
-	}
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getName() {
-		return $this->name;
-	}
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getLocalizedName() {
-		return $this->countryService->getLocalizedName($this);
-	}
+    /**
+     * @return string
+     */
+    public function getLocalizedName()
+    {
+        return $this->countryService->getLocalizedName($this);
+    }
 
-	/**
-	 * @param string $commonName
-	 */
-	public function setCommonName($commonName) {
-		$this->commonName = $commonName;
-	}
+    /**
+     * @param string $commonName
+     */
+    public function setCommonName($commonName)
+    {
+        $this->commonName = $commonName;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getCommonName() {
-		return $this->commonName;
-	}
+    /**
+     * @return string
+     */
+    public function getCommonName()
+    {
+        return $this->commonName;
+    }
 
-	/**
-	 * @param string $officialName
-	 */
-	public function setOfficialName($officialName) {
-		$this->officialName = $officialName;
-	}
+    /**
+     * @param string $officialName
+     */
+    public function setOfficialName($officialName)
+    {
+        $this->officialName = $officialName;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getOfficialName() {
-		return $this->officialName;
-	}
+    /**
+     * @return string
+     */
+    public function getOfficialName()
+    {
+        return $this->officialName;
+    }
 
-	/**
-	 * @param string $numericCode
-	 */
-	public function setNumericCode($numericCode) {
-		$this->numericCode = $numericCode;
-	}
+    /**
+     * @param string $numericCode
+     */
+    public function setNumericCode($numericCode)
+    {
+        $this->numericCode = $numericCode;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getNumericCode() {
-		return $this->numericCode;
-	}
+    /**
+     * @return string
+     */
+    public function getNumericCode()
+    {
+        return $this->numericCode;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString() {
-		return $this->alpha2;
-	}
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        if (is_string($this->alpha2)) {
+            return $this->alpha2;
+        }
+        if (is_string($this->alpha3)) {
+            return $this->alpha3;
+        }
+        if (is_string($this->alpha4)) {
+            return $this->alpha4;
+        }
+        return '';
+    }
 
 }
-
-?>
